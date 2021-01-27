360c35431ad0bc070a9c2786c9e31afc14ff547e
KaanDoesNothing <kaanyt123@gmail.com>
2021-01-27 20:43:17 +0100

    Optimizing some stuff and adding the ability to use it without js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ let browser;
 const app = express();
 
 app.set("view engine", "pug");
+app.use("/static", express.static("./public"));
 
 app.get("/", async (req, res) => {
     let {q} = req.query;
