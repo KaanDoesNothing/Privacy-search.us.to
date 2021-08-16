aaf84c00e83b940892ca231a170e19f6b9584e6a
KaanDoesNothing <kaanyt123@gmail.com>
2021-08-16 14:29:18 +0200

    Little fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 export const takeScreenshot = async (url: string) => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]});
     const page = (await browser.pages())[0];
 
     await page.goto(fixURL(url), { waitUntil: "networkidle2", timeout: 60000 });
