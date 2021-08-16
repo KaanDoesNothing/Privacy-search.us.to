27356e70c088d3874e2cd9d2954b4cdc3d6e1daf
KaanDoesNothing <kaanyt123@gmail.com>
2021-08-16 19:01:39 +0200

    Ad blocking

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,21 @@
 import puppeteer from "puppeteer";
+import crossfetch from "cross-fetch";
+import { PuppeteerBlocker } from "@cliqz/adblocker-puppeteer";
+import applyEvasions from "./lib/applyEvasions";
+
+let blocker;
+
+//Initialize the blocker
+(async () => {
+    blocker = await PuppeteerBlocker.fromPrebuiltAdsAndTracking(crossfetch);
+})();
 
 export const takeScreenshot = async (url: string) => {
     const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]});
     const page = (await browser.pages())[0];
+
+    await blocker.enableBlockingInPage(page);
+    await applyEvasions(page);
 
     await page.goto(fixURL(url), { waitUntil: "networkidle2", timeout: 60000 });
 
